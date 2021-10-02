99848a4ace7be36fa5289c518c9ad09cc26220b2
Hendrix-Shen <HendrixShen@qq.com>
2021-10-02 15:32:50 +0800

    Fix server crash when player join

--- src/main/java/top/hendrixshen/SyncmaticaPatches/mixin/MixinSyncmatica.java
+++ src/main/java/top/hendrixshen/SyncmaticaPatches/mixin/MixinSyncmatica.java
@@ -1,40 +1,38 @@
 package top.hendrixshen.SyncmaticaPatches.mixin;
 
 import ch.endte.syncmatica.Context;
+import ch.endte.syncmatica.IFileStorage;
+import ch.endte.syncmatica.SyncmaticManager;
 import ch.endte.syncmatica.Syncmatica;
+import ch.endte.syncmatica.communication.CommunicationManager;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import top.hendrixshen.SyncmaticaPatches.Config;
-import top.hendrixshen.SyncmaticaPatches.SyncmaticaPatches;
 
 import java.io.File;
 
 @Mixin(value = Syncmatica.class, remap = false)
 public abstract class MixinSyncmatica {
     @Shadow
-    private static void init(Context con, Identifier contextId) {
-    }
-
-    @Shadow
     @Final
     private static String SERVER_PATH;
 
-    @Redirect(
-            method = "initServer",
-            at = @At(
-                    value = "NEW"
-            )
-    )
-    private static File redirectServerDataPath() {
+    @Shadow
+    private static void init(Context con, Identifier contextId) {
+    }
+
+    /**
+     * @author Hendrix-Shen
+     * @reason Modify server path
+     */
+    @Overwrite
+    public static void initServer(final CommunicationManager comms, final IFileStorage fileStorage, final SyncmaticManager schematics) {
         File file = new File(Config.config.get("serverPath"));
-        if (file.exists()) {
-            return file;
-        }
-        SyncmaticaPatches.logger.error(String.format("File not found: %s", file));
-        return new File(SERVER_PATH);
+        file = file.isDirectory() ? file : new File(SERVER_PATH);
+        final Context serverContext = new Context(fileStorage, comms, schematics, true, file);
+        init(serverContext, Syncmatica.SERVER_CONTEXT);
     }
 }
